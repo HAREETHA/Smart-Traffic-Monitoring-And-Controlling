5fd5ccbfaca04d8a01d545e7d4c39e90869ec311
HAREETHA <47220322+HAREETHA@users.noreply.github.com>
2021-05-06 17:27:13 +0530

    Add files via upload

---
+++ YOLO/app/src/main/java/com/bit/yolo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.bit.yolo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
